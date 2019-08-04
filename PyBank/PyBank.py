fd1d1be325b25534ffaf1fe1544cfa16c6adc9fb
setsumei1974 <51409139+setsumei1974@users.noreply.github.com>
2019-08-03 20:50:22 -0500

    PyBank - Final

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -9,60 +9,50 @@ with open(csvpath, newline="") as csvfile:
 
     MonthsTotal = 0
     NetProfitLoss = 0
+    ChangeByDate = []
+    ChangePLPerMonth = []
+    
+#Loop to calculate the total number of months in the first column and the net profit and loss of the second column
+#With the creation of lists in the first set of variables, the lists will be appended with information from the columns in order to prepare for the next loop to determine the average of the changes in profit and loss
 
-    #Find total months and total profits
     for row in csvreader:
         MonthsTotal += 1
         NetProfitLoss += int(row[1])
-
-  #  print(MonthsTotal)
-  #  print(NetProfitLoss)
-
-    BaseRow = int(row[1])
+        ChangeByDate.append(row[0])
+        ChangePLPerMonth.append(int(row[1]))
+        
+        
     TotalChangePL = []
-
-    print(BaseRow)
-    print(TotalChangePL)
-#change per month, creation of a list of changes
-  #  for row in csvreader:
-  #     ChangePLPerMonth = int(row[1]) - BaseRow
-  #     TotalChangePL.append(ChangePLPerMonth)
-  #      BaseRow = int(row[1])
-
-  #  AverageChangePL = sum(TotalChangePL)/ len(TotalChangePL)
-    AverageChangePL = NetProfitLoss / MonthsTotal
-
-    print(AverageChangePL)
     
-    MaxIncreaseAmount = 0
-    MaxIncreaseDate = 0
-    MaxDecreaseAmount = 0
-    MaxDecreaseDate = 0
+ #Append individual changes in profit and loss to a list for total changes of profit and loss
+ #Calculate average of the changes
 
-    for row in csvreader:
-        if int(row[1]) > MaxIncreaseAmount:
-            MaxIncreaseAmount = int(row[1])
-            MaxIncreaseDate = row[0]
+    for i in range(MonthsTotal - 1):
+        TotalChangePL.append(int(ChangePLPerMonth[i+1]) - int(ChangePLPerMonth[i]))
+        AverageChangePL = round(sum(TotalChangePL)/len(TotalChangePL), 2)
 
-        if int(row[1]) < MaxDecreaseAmount:
-            MaxDecreaseAmount = int(row[1])
-            MaxDecreaseDate = row[0]
+#Use list of total changes to determine maximum amounts and dates
 
+MaxIncreaseAmount = max(TotalChangePL)
+MaxDecreaseAmount = min(TotalChangePL)
+MaxIncreaseDate = ChangeByDate[TotalChangePL.index(max(TotalChangePL))+1]
+MaxDecreaseDate = ChangeByDate[TotalChangePL.index(min(TotalChangePL))+1]
+ 
 print("Financial Analysis")
-print(_________________________)
-print("Total Months: " + MonthsTotal)
-print("Total: " + NetProfitLoss)
-print("Average Change: " + AverageChangePL)
-print("Greatest Increase in Profits: " + MaxIncreaseDate + " with " + MaxIncreaseAmount)
-print("Greatest Decrease in Profits: " + MaxDecreaseDate + " with " + MaxDecreaseAmount)
+print("-------------------------")
+print(f"Total Months: {MonthsTotal}")
+print(f"Total: {NetProfitLoss}")
+print(f"Average Change: {AverageChangePL}")
+print(f"Greatest Increase in Profits: {MaxIncreaseDate} with {MaxIncreaseAmount}")
+print(f"Greatest Decrease in Profits: {MaxDecreaseDate} with {MaxDecreaseAmount}")
 
-output_file = os.path.join("BudgetDataRevised.csv")
+output_file = os.path.join("..", "BudgetDataRevised.csv")
 
 with open(output_file, "w",) as txtfile:
-    txtfile.write("Financial Analysis" , "\n")
-    txtfile.write("_________________________" , "\n")
-    txtfile.write("Total Months: " + MonthsTotal , "\n")
-    txtfile.write("Total: " + NetProfitLoss , "\n")
-    txtfile.write("Average Change: " + AverageChangePL , "\n")
-    txtfile.write("Greatest Increase in Profits: " + MaxIncreaseDate + " with " + MaxIncreaseAmount , "\n")
-    txtfile.write("Greatest Decrease in Profits: " + MaxDecreaseDate + " with " + MaxDecreaseAmount , "\n")
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("-------------------------\n")
+    txtfile.write("Total Months: " + str(MonthsTotal)  + "\n")
+    txtfile.write("Total: " + str(NetProfitLoss) + "\n")
+    txtfile.write("Average Change: " + str(AverageChangePL) + "\n")
+    txtfile.write("Greatest Increase in Profits: " + str(MaxIncreaseDate) + " with " + str(MaxIncreaseAmount) + "\n")
+    txtfile.write("Greatest Decrease in Profits: " + str(MaxDecreaseDate) + " with " + str(MaxDecreaseAmount) + "\n")      
